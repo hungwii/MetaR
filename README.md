94492952bd756d27ea3f08850828d82fc17edc93
hungwii <hungwii@163.com>
2021-09-27 12:26:49 +0000

    git版本控制

---
+++ README.md
@@ -0,0 +1,29 @@
+## MetaR
+主函数有两个：main.py和main_output_rel.py.
+
+main.py: 跑模型
+main_output.py:调用跑好的模型进行验证效果
+
+具体的命令看印象笔记中的记录
+
+
+
+## Cite
+
+```
+@inproceedings{chen-etal-2019-meta,
+    title     = "Meta Relational Learning for Few-Shot Link Prediction in Knowledge Graphs",
+    author    = "Chen, Mingyang  and
+                 Zhang, Wen  and
+                 Zhang, Wei  and
+                 Chen, Qiang  and
+                 Chen, Huajun",
+    booktitle = "Proceedings of the 2019 Conference on Empirical Methods in Natural Language Processing and the 9th International Joint Conference on Natural Language Processing (EMNLP-IJCNLP)",
+    month     = nov,
+    year      = "2019",
+    address   = "Hong Kong, China",
+    publisher = "Association for Computational Linguistics",
+    url       = "https://www.aclweb.org/anthology/D19-1431",
+    pages     = "4208--4217"
+}
+```
---
+++ main_output_rel_old.py
@@ -0,0 +1,158 @@
+from trainer import *
+from params import *
+from data_loader import *
+import json
+import random
+import numpy as np
+
+'''
+这个主函数的作用主要是根据已经训练好的模型，输入test_task数据集，然后输出分数，以便用于分析。中间设置了很多变量用于检查数据的形式
+'''
+
+if __name__ == '__main__':
+    params = get_params()
+
+    print("---------Parameters---------")
+    for k, v in params.items():
+        print(k + ': ' + str(v))
+    print("----------------------------")
+
+    # control random seed
+    if params['seed'] is not None:
+        SEED = params['seed']
+        torch.manual_seed(SEED)
+        torch.cuda.manual_seed(SEED)
+        torch.backends.cudnn.deterministic = True
+        np.random.seed(SEED)
+        random.seed(SEED)
+
+    # select the dataset
+    for k, v in data_dir.items():
+        data_dir[k] = params['data_path']+v
+
+    tail = ''
+    if params['data_form'] == 'In-Train':
+        tail = '_in_train'
+
+    dataset = dict()
+    print("loading train_tasks{} ... ...".format(tail))
+    dataset['train_tasks'] = json.load(open(data_dir['train_tasks'+tail]))
+    print("loading test_tasks ... ...")
+    dataset['test_tasks'] = json.load(open(data_dir['test_tasks']))
+    print("loading dev_tasks ... ...")
+    dataset['dev_tasks'] = json.load(open(data_dir['dev_tasks']))
+    print("loading rel2candidates{} ... ...".format(tail))
+    dataset['rel2candidates'] = json.load(open(data_dir['rel2candidates'+tail]))
+    print("loading e1rel_e2{} ... ...".format(tail))
+    dataset['e1rel_e2'] = json.load(open(data_dir['e1rel_e2'+tail]))
+    print("loading ent2id ... ...")
+    dataset['ent2id'] = json.load(open(data_dir['ent2ids']))
+
+    if params['data_form'] == 'Pre-Train':
+        print('loading embedding ... ...')
+        dataset['ent2emb'] = np.load(data_dir['ent2vec'])
+
+    print("----------------------------")
+
+    # data_loader
+    train_data_loader = DataLoader(dataset, params, step='train')
+    dev_data_loader = DataLoader(dataset, params, step='dev')
+    test_data_loader = DataLoader(dataset, params, step='test')
+    data_loaders = [train_data_loader, dev_data_loader, test_data_loader]
+
+    # trainer
+    trainer = Trainer(data_loaders, dataset, params)
+
+    if params['step'] == 'train':
+        trainer.train()
+        print("test") 
+        print(params['prefix'])
+        trainer.reload()
+        trainer.eval(istest=True)
+    elif params['step'] == 'test':
+        print(params['prefix'])
+        if params['eval_by_rel']:
+            trainer.eval_by_relation(istest=True)
+        else:
+            trainer.eval(istest=True)
+
+        all_rel_dic = trainer.metaR.rel_q_sharing
+
+        for _ in all_rel_dic.keys():
+            print('----------------')
+            print(_)
+            print('----------------')
+            triples = dataset['test_tasks'][_] #导入当前rel的三元组
+            #生成负样本
+            negative = []
+            e1rel_e2, curr_cand = dataset['e1rel_e2'], dataset['rel2candidates'][_]
+
+            #把当前关系下的节点进行分类，方便排列组合
+            compounds, genes = [], []
+            for cand in curr_cand:
+                if cand[0] == 'C':
+                    compounds.append(cand)
+                if cand[0] == 'G':
+                    genes.append(cand)
+            
+            #进行排列组合
+            #TODO:源代码进行h+r-t的时候，有没有考虑到绝对值的问题？
+            for c in compounds:
+                for g in genes:
+                    if ([c, _, g] or [g, _, c]) in triples:
+                        continue
+                    else:
+                        negative.append([c, _, g])
+                
+            print('正样本的个数', len(triples))
+            print(triples[0])
+            print('负样本的个数', len(negative))
+            print(negative[0])
+            #把正负样本传给meta。embedding,得到对应的embedding
+            # triples_emb, negative_emb = [trainer.metaR.embedding(t) for t in [triples, negative]
+            triples_emb = trainer.metaR.embedding([triples])
+            negative_emb = trainer.metaR.embedding([negative])
+
+            #把正样本和负样本的h和t用split.concat()函数连接在一起
+            head_emb, tail_emb = trainer.metaR.split_concat(triples_emb, negative_emb)
+
+            #获取关系rel的embedding
+            rel_emb = all_rel_dic[_].expand(-1,(len(triples) + len(negative)), -1, -1)
+            print('----------------------')
+
+            print('head_emb:', head_emb.shape)
+            print('tail_emb:', tail_emb.shape)
+            print('rel_emb:', rel_emb.shape)
+            #计算分数，p_score是正样本的分数，n_score是负样本的分数
+            p_score, n_score = trainer.metaR.embedding_learner(head_emb, tail_emb, rel_emb, len(triples))
+
+            #把分数进行排名，并且组合成我要的输出形式
+            yes = ['yes' for i in range(len(triples))]
+            no = ['no' for j in range(len(negative))]
+
+            all_triples = triples + negative
+            print('p_score:', p_score.shape)
+            print('n_score:', n_score.shape)
+            
+            input('stop a moment')
+            all_scores = (torch.cat((p_score, n_score),1).squeeze()).cpu().detach().numpy().tolist()
+            all_imfo = yes + no
+            zipped = zip(all_triples, all_scores, all_imfo)
+            my_sort = list(sorted(zipped, key=lambda x: x[1], reverse=True))
+
+            #写成文件
+            with open('./test_result/wiki_0807.txt', 'a', encoding='utf-8') as output:
+                for row in my_sort:
+                    rowtext = '{} {} {} {} {}'.format(row[0][1], row[0][0], row[0][2], row[1], row[2])
+                    output.write(rowtext)
+                    output.write('\n')
+                output.write('\n')
+                output.write('--------------------------')
+         
+    elif params['step'] == 'dev':
+        print(params['prefix'])
+        if params['eval_by_rel']:
+            trainer.eval_by_relation(istest=False)
+        else:
+            trainer.eval(istest=False)
+
